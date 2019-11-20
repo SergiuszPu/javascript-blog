9093d78045c1649103d83f2cdeb707ed856da690
Sergiusz Pu <serson85@gmail.com>
2019-11-20 19:20:08 +0100

    add all things

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 const titleClickHandler = function (event) {
     console.log('Link was clicked!');
     console.log(event);
-
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
-
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-
     /* add class 'active' to the clicked link */
-
+    const clickedElement = this;
+    clickedElement.classList.add('active');
+    console.log('clickedElement (with plus): ' + clickedElement);
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.article a.active');
-
     for (let activeArticle of activeArticles) {
-        activeLink.classList.remove('active');
+        activeArticle.classList.remove('active');
     }
-
     /* get 'href' attribute from the clicked link */
-
+    const articleSelector = clickedElement.getAttribute('href')
+    console.log(articleSelector);
     /* find the correct article using the selector (value of 'href' attribute) */
-
+    const targetArticle = document.querySelector(articleSelector)
+    console.log(targetArticle)
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active')
 }
 
 const links = document.querySelectorAll('.titles a');
