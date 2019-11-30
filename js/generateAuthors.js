d9239e63218e397a2c57a7dcea95ec0f83fc6b64
Sergiusz Pu <serson85@gmail.com>
2019-11-30 17:40:32 +0100

    new thigs

---
+++ js/generateAuthors.js
@@ -0,0 +1,13 @@
+optArticleAuthorSelector = '.list.authors a';
+
+function generateAuthors () {
+
+const authorsArticle = querySelectoreAll(optArticleAuthorSelector);
+console.log(authorsArticle);
+
+
+
+
+
+    
+}
